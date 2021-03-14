05dde620bbe15129988272950aa39f6fba9fc9e3
Arni Magnusson <thisisarni@gmail.com>
2021-03-14 15:33:17 +0100

    model.R reads from data, writes to model

--- area37/model.R
+++ area37/model.R
@@ -1,14 +1,20 @@
 ## Run analysis, write model results
 
-## Before:
-## After:
+## Before: input.rds (data)
+## After:  results.rds (model)
 
 library(icesTAF)
+library(dplyr)   # filter, mutate, sample_n, ungroup
+library(purrr)   # map, map_lgl, safely
+library(sraplus) # diagnose_sraplus, fit_sraplus, summarize_sralpus
 
 mkdir("model")
 
+nested_indo <- readRDS("data/input.rds")
+
 ## ----fit-sraplus----------------------------------------------------------------------------------
 
+setwd("model") # compile inside 'model' folder
 sfs <- safely(fit_sraplus)
 samps <-30
 a = Sys.time()
@@ -30,37 +36,23 @@ nested_indo <- nested_indo %>%
       estimate_qslope = FALSE,
       estimate_proc_error = TRUE)
     ))
-
+setwd("..")
 
 Sys.time() - a
 
-  nested_indo <- nested_indo %>%
+nested_indo <- nested_indo %>%
   mutate(sraplus_worked = map_lgl(map(sraplus_fit,"error"), is.null)) %>%
   filter(sraplus_worked) %>%
   mutate(sraplus_fit = map(sraplus_fit,"result"))
-
-
-plot_driors(nested_indo$driors[[1]])
-
-plot_prior_posterior(nested_indo$sraplus_fit[[1]], nested_indo$driors[[1]])
-
-plot_prior_posterior(nested_indo$sraplus_fit[[1]], nested_indo$driors[[1]])
-
-
-plot(nested_indo$driors[[1]]$catch[nested_indo$driors[[1]]$years %in% nested_indo$driors[[1]]$effort_years] / nested_indo$driors[[1]]$effort)
-
-
 
 ## -------------------------------------------------------------------------------------------------
 
 nested_indo <- nested_indo %>%
   mutate(sraplus_summary = map(sraplus_fit, sraplus::summarize_sralpus))
 
-
-
 ## -------------------------------------------------------------------------------------------------
 
 nested_indo <- nested_indo %>%
   mutate(sraplus_diagnostics = map2(sraplus_fit, driors, diagnose_sraplus))
 
-save(nested_indo,file="C:/Users/rishi/Documents/Area37Marcelo/results/Area37NominalEffortAggSOFIAdata.RData")
+saveRDS(nested_indo, "model/results.rds")
