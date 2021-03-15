3a17a75a616e02bb56872f3b51ba4fd48137c06e
Arni Magnusson <thisisarni@gmail.com>
2021-03-15 13:25:43 +0100

    output.R reads from model, writes to output

--- area37/output.R
+++ area37/output.R
@@ -1,57 +1,52 @@
 ## Extract results of interest, write TAF output tables
 
-## Before:
-## After:
+## Before: results.rds (model)
+## After:  stock_tables/*.csv, all_effort.txt, bbmsy.png, cpue_1.png,
+##         driors_1.png, posterior_1.png, proportions.png, status_sofia.png,
+##         status.png, stock_posterior.pdf, stock_timeseries.pdf (output)
 
 library(icesTAF)
+library(dplyr)        # case_when, count, group_by, select, filter, mutate
+library(egg)          # ggarrange
+library(ggplot2)
+library(purrr)        # map2, walk2
+library(sraplus)      # plot_driors, plot_prior_posterior, plot_sraplus
+library(tidyr)        # pivot_wider, unnest
+source("utilities.R") # plotProp
 
 mkdir("output")
 
-plot_driors(nested_indo$driors[[1]])
+nested_indo <- readRDS("model/results.rds")
+
+plot_driors(nested_indo$driors[[1]]) # stock 1 is Sardinella aurita
+ggsave("output/driors_1.png")
 
 plot_prior_posterior(nested_indo$sraplus_fit[[1]], nested_indo$driors[[1]])
+ggsave("output/posterior_1.png")
 
-plot_prior_posterior(nested_indo$sraplus_fit[[1]], nested_indo$driors[[1]])
-
-
+taf.png("output/cpue_1.png")
 plot(nested_indo$driors[[1]]$catch[nested_indo$driors[[1]]$years %in% nested_indo$driors[[1]]$effort_years] / nested_indo$driors[[1]]$effort)
-
-
-
-## -------------------------------------------------------------------------------------------------
-
-nested_indo <- nested_indo %>%
-  mutate(sraplus_summary = map(sraplus_fit, sraplus::summarize_sralpus))
-
-
+# with(nested_indo$driors[[1]], plot(catch[years %in% effort_years] / effort))
+dev.off()
 
 ## -------------------------------------------------------------------------------------------------
-
-nested_indo <- nested_indo %>%
-  mutate(sraplus_diagnostics = map2(sraplus_fit, driors, diagnose_sraplus))
-
-save(nested_indo,file="C:/Users/rishi/Documents/Area37Marcelo/results/Area37NominalEffortAggSOFIAdata.RData")
 
 # nested_indo <- nested_indo %>%
 #   filter(map_lgl(
 #     map_chr(sraplus_diagnostics, "fit_diagnostic_message"),
 #     ~ .x == "There is no evidence that the model is not converged"
 #   ))
-#
-nsamps<-25
-for(i in 1:(nsamps-1)){
 
-a = sraplus::summarize_sralpus(nested_indo$sraplus_fit[[i]])
-
-a <- nested_indo$sraplus_fit[[i]]$results
-
-a %>%
-  filter(variable == "index_hat_t")
-#plot_prior_posterior(nested_indo$sraplus_fit[[i]], #nested_indo$driors[[i]])
-
-  plot_sraplus(nested_indo$sraplus_fit[[i]])
-}
-
+# This plot is done more efficiently later, see stock_timeseries.pdf
+# nsamps<-25
+# for(i in 1:(nsamps-1)){
+#   # a = sraplus::summarize_sralpus(nested_indo$sraplus_fit[[i]])
+#   a <- nested_indo$sraplus_fit[[i]]$results
+#   a %>%
+#     filter(variable == "index_hat_t")
+#   # plot_prior_posterior(nested_indo$sraplus_fit[[i]], #nested_indo$driors[[i]])
+#   print(plot_sraplus(nested_indo$sraplus_fit[[i]]))
+# }
 
 ## -------------------------------------------------------------------------------------------------
 
@@ -63,48 +58,51 @@ indo_results <- nested_indo %>%
                                    mean > 0.8 ~ "fully fished",
                                    TRUE ~ "overfished"))
 
-
 indo_results %>%
   group_by(status) %>%
   count()
 
-
-
-
 ## -------------------------------------------------------------------------------------------------
 
+taf.png("output/status.png")
 resultsindo<-c(14/25,11/25,0/25)
 names(resultsindo)=c("fullyfished","overfished","underfished")
 barplot(resultsindo,col=c("yellow","red","green"))
+dev.off()
 
+taf.png("output/status_sofia.png")
 resultsindoSOFIA<-c(0.35,0.625,0.025)
 names(resultsindoSOFIA)=c("fullyfished","overfished","underfished")
 barplot(resultsindoSOFIA,col=c("yellow","red","green"))
-
-
+dev.off()
 
 ## -------------------------------------------------------------------------------------------------
 
 nested_indo <- nested_indo %>%
   #mutate(sraplus_fit_plot = map(sraplus_fit, plot_sraplus))
-   mutate(plot_prior_posterior_plot=map2(sraplus_fit,driors,plot_prior_posterior))
-savefoo <- function(stock,plot, extension = "pdf"){
+  mutate(plot_prior_posterior_plot=map2(sraplus_fit,driors,plot_prior_posterior))
 
-  ggsave(filename = paste(stock,extension,sep = "."), plot = plot + labs(title = stock))
-
+savefoo <- function(stock, plot){
+  print(plot + labs(title = stock))
 }
-walk2(nested_indo$stock,nested_indo$plot_prior_posterior_plot,savefoo)
+
+pdf("output/stock_posterior.pdf")
+walk2(nested_indo$stock, nested_indo$plot_prior_posterior_plot, savefoo)
+dev.off()
+
 #walk2(nested_indo$stock,nested_indo$sraplus_fit_plot, savefoo)
 # setwd("C:/Users/rishi/Documents/IndonesiaSDGStkStatus")
 #for(i in 1:nrow(nested_indo)){
 #out<-write.csv(nested_indo$sraplus_summary[i],file=paste(nested_indo$stock[i],".csv",sep=""))}
 
+mkdir("output/stock_tables")
 for(i in 1:nrow(nested_indo)){
-out<-write.csv(nested_indo$sraplus_fit[i][[1]]$results,file=paste(nested_indo$stock[i],".csv",sep=""))
-out2<-write.table(nested_indo$sraplus_fit[i][[1]]$results,file="resultsallArea57sumeffort",append=T)}
-
+  out<-write.csv(nested_indo$sraplus_fit[i][[1]]$results,file=paste0("output/stock_tables/",nested_indo$stock[i],".csv"))
+  out2<-write.table(nested_indo$sraplus_fit[i][[1]]$results,file="output/all_effort.txt",append=T)
+}
 
 ## -------------------------------------------------------------------------------------------------
+
 #save(nested_indo,file="C:/Users/rishi/Documents/Area37Marcelo/results/Area37NominalEffort.RData")
 
 #Area37Results=ldply(nested_indo$sraplus_fit[[i]]$results,function(x){
@@ -113,25 +111,23 @@ out2<-write.table(nested_indo$sraplus_fit[i][[1]]$results,file="resultsallArea57
 
 #save(sraresultsscenario3,file="C:/Users/rishi/Documents/ICEScompsdatasetRainer/results/icesSRAResultsscenario3Jun30.RData")
 
-
-
-
 ## -------------------------------------------------------------------------------------------------
 
 nested_indo <- nested_indo %>%
   mutate(sraplus_fit_plot = map(sraplus_fit, plot_sraplus))
 
-savefoo <- function(stock,plot, extension = "pdf"){
-
-  ggsave(filename = paste(stock,extension,sep = "."), plot = plot + labs(title = stock))
-
+savefoo <- function(stock, plot){
+  print(plot + labs(title = stock))
 }
 
+pdf("output/stock_timeseries.pdf")
 walk2(nested_indo$stock,nested_indo$sraplus_fit_plot, savefoo)
-setwd("C:/Users/rishi/Documents/Area67TestsUW")
-for(i in 1:(nsamps-1)){
-out<-write.csv(nested_indo$sraplus_fit[[i]]$results,file=paste(nested_indo$stock[i],".csv",sep=""))}
+dev.off()
 
+# These tables were already created, see stock_tables
+# setwd("C:/Users/rishi/Documents/Area67TestsUW")
+# for(i in 1:(nsamps-1)){
+# out<-write.csv(nested_indo$sraplus_fit[[i]]$results,file=paste(nested_indo$stock[i],".csv",sep=""))}
 
 ## -------------------------------------------------------------------------------------------------
 
@@ -140,38 +136,29 @@ nested_indo <- nested_indo %>%
 
 nested_indo$sraplus_diagnostics[[1]]$final_gradient
 
+## -------------------------------------------------------------------------------------------------
 
+# Simple way..Run
+# setwd("C:/Users/rishi/Documents/Area67TestsUW/cpueeffforcedpriorfits")
+# ResultsArea67<-read.csv("ResultsArea67EffectiveeffortInformativePrior.csv")
+# p1 <- plotProp(ResultsArea67,method="area67.effEdepP",cats=3, type="prop")
+# p2 <- plotProp(ResultsArea67,method="area67.effEdepP",cats=3, type="all")
+# ggarrange(p1,p2,ncol=1)
 
 ## -------------------------------------------------------------------------------------------------
 
-#Simple way..Run
-library(egg)
-setwd("C:/Users/rishi/Documents/Area67TestsUW/cpueeffforcedpriorfits")
-ResultsArea67<-read.csv("ResultsArea67EffectiveeffortInformativePrior.csv")
-p1 <- plotProp(ResultsArea67,method="area67.effEdepP",cats=3, type="prop")
-p2 <- plotProp(ResultsArea67,method="area67.effEdepP",cats=3, type="all")
-
-ggarrange(p1,p2,ncol=1)
-
-
-## -------------------------------------------------------------------------------------------------
-
-##Summary Plot after getting data sorted###
-library(reshape)
-library(plyr)
-library(dplyr)
-setwd("C:/Users/rishi/Documents/Area37Marcelo/sraplusfitfinerresolutionfigs")
-Area37<-read.csv("Area67Effectiveeffortforcedpriors.csv")
-meltdf <- melt(Area67,id="Year")
-sp<-ggplot(meltdf,aes(x=Year,y=value,colour=variable,group=variable)) + geom_line(show.legend = T)
-sp2<-sp + geom_hline(yintercept=0.8, linetype="dashed",
-                color = "red", size=2)
-sp2 + geom_hline(yintercept=1.2, linetype="dashed",
-                color = "green", size=2, show.legend = FALSE)
+# Summary Plot after getting data sorted###
+# setwd("C:/Users/rishi/Documents/Area37Marcelo/sraplusfitfinerresolutionfigs")
+# Area37<-read.csv("Area67Effectiveeffortforcedpriors.csv")
+# meltdf <- melt(Area67,id="Year")
+# sp<-ggplot(meltdf,aes(x=Year,y=value,colour=variable,group=variable)) + geom_line(show.legend = T)
+# sp2<-sp + geom_hline(yintercept=0.8, linetype="dashed",
+#                      color = "red", size=2)
+# sp2 + geom_hline(yintercept=1.2, linetype="dashed",
+#                  color = "green", size=2, show.legend = FALSE)
 
 ## -------------------------------------------------------------------------------------------------
 
-library(reshape)
 n<-length(nested_indo$stock)
 resList <- vector(mode="list",length=n)
 for(i in 1:n){
@@ -186,16 +173,14 @@ names(newResTab)=c("Stock","yr","bbmsy","ffmsy")
 newResTab$bbmsy.effEdepP<-newResTab$bbmsy
 newResTab$ffmsy.effEdepP<-newResTab$ffmsy
 
-library(ggplot2)
-library(egg)
+taf.png("output/proportions.png")
 p1 <- plotProp(newResTab,method="effEdepP",cats=3, type="prop")
 p2 <- plotProp(newResTab,method="effEdepP",cats=3, type="all")
-
 ggarrange(p1,p2,ncol=1)
+dev.off()
 
-####
-library(ggplot2)
 ggplot(newResTab,aes(x=yr,y=bbmsy,colour=Stock,group=Stock)) +
   geom_line(show.legend = T) +
   geom_hline(yintercept=0.8, linetype="dashed",color = "red", size=2) +
   geom_hline(yintercept=1.2, linetype="dashed",color = "green", size=2)
+ggsave("output/bbmsy.png")
